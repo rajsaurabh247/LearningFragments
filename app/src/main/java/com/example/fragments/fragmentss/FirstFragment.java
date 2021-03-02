1d66516223c6470537461199d931c8a16529ffae
Saurabh Raj <rajsaurabh247@gmail.com>
2021-03-02 13:06:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragments/fragmentss/FirstFragment.java
@@ -0,0 +1,28 @@
+package com.example.fragments.fragmentss;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.fragments.R;
+
+public class FirstFragment extends Fragment {
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_first, container, false);
+
+
+
+        return view;
+    }
+}
